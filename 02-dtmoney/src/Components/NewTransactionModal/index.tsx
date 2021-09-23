5a9d50f62d5189acebc1d1cdcaf9242b05ba3856
Pedro Frá <pedro_delavald@hotmail.com>
2021-09-23 15:48:25 -0300

    Criação do fomrulário do modal e estilização das classes dele no global.ts

--- 02-dtmoney/src/Components/NewTransactionModal/index.tsx
+++ 02-dtmoney/src/Components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal'
+import {Container} from './styles'
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -10,8 +11,27 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <Modal 
+    isOpen={isOpen} 
+    onRequestClose={onRequestClose}
+    overlayClassName='react-modal-overlay'
+    className='react-modal-content'
+    >
+      <Container>
+
       <h2>Cadastro de Nova Transação</h2>
+
+      <input placeholder="Título" />
+
+      <input type="number" placeholder="Valor"/>
+
+      <input placeholder="Categoria" />
+
+      <button type="submit">Cadastrar</button>
+
+
+      </Container>
+
     </Modal>
   )
 }
