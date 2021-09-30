ceef9a625d5c7bf107a55b0b82cf0154433cd302
Pedro Frá <pedro_delavald@hotmail.com>
2021-09-30 15:28:46 -0300

    Alteração nas rotas e implementação do schema do banco de dados

--- 02-dtmoney/src/Components/NewTransactionModal/index.tsx
+++ 02-dtmoney/src/Components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import closeImg from '../../assets/close.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import incomeImg from '../../assets/income.svg'
 import { FormEvent, useState } from 'react'
+import { api } from '../../services/api'
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -21,7 +22,19 @@ export function NewTransactionModal({
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
+  
+
+  const data = {
+    title,
+    value,
+    category,
+    type,
   }
+
+  api.post('/transactions', data)
+
+  }
+
 
   return (
     <Modal
